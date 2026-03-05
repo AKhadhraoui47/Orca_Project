9666647c5a22116c59ec85e91d1707ddb97e3e62
mohamed bilel khadhraoui <bkhadhraoui870@gmail.com>
2026-03-06 00:20:15 +0100

    u-boot-stm32mp/tilikum_common.h: Updated bootcmd_tilikum

--- meta-orca-bsp/recipes-bsp/uboot/u-boot-stm32mp/tilikum_common.h
+++ meta-orca-bsp/recipes-bsp/uboot/u-boot-stm32mp/tilikum_common.h
@@ -2,6 +2,6 @@
 #define __TILIKUM_COMMON_H__
 
 #define TILIKUM_BOOTCOMMAND "bootcmd_tilikum=" \
-	"echo \"Boot over ###### MMC ######\";\0"
+	"sysboot mmc 0:8 any 0xc6100000 /extlinux/extlinux.conf;\0"
 
 #endif
